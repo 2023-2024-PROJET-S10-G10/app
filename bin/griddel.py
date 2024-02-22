73d2e9406ba882109a1677703ced09386ab4c350
Diego CORREIA DE OLIVEIRA <Diego.cdo27@gmail.com>
2024-02-22 11:08:23 +0100

    Init: Creating the binaries for the grid command with a light implementation of the CLI option

---
+++ bin/griddel.py
@@ -0,0 +1,66 @@
+import rich_click as click
+
+
+class IntListParamType(click.ParamType):
+    name = "id1,id2,idN"
+
+    def convert(self, value, param, ctx):
+        try:
+            ids = [int(x.strip()) for x in value.split(',')]
+            return ids
+        except ValueError:
+            self.fail(f"{value} is not a valid list of integers", param, ctx)
+
+
+class ID(int):
+    name = "ID"
+
+
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.option(
+    "-c", "--campaign_ids",
+    type=IntListParamType(),
+    default=None,
+    help="Campaigns on which to act"
+)
+@click.option(
+    "-p", "--pause",
+    default=False,
+    is_flag=True,
+    help="Holds the campaign"
+)
+@click.option(
+    "-r", "--resume",
+    default=False,
+    is_flag=True,
+    help="Resumes the campaign (only if it is paused)"
+)
+@click.option(
+    "--purge",
+    default=False,
+    is_flag=True,
+    help="Purge the campaign (only if it is finished)"
+)
+@click.option(
+    "-j", "--job",
+    type=ID,
+    default=None,
+    help="Cancel a single job"
+)
+@click.option(
+    "-v", "--verbose",
+    default=False,
+    show_default=True,
+    is_flag=True,
+    help="Enable verbose mode"
+)
+@click.version_option("4.0.0", prog_name="CiGri")
+def cli(campaign_ids, pause, resume, purge, job, verbose):
+    """
+    This command allow the user to manage the already existing campaigns.
+    """
+    raise RuntimeError("NYI")
+
+
+if __name__ == "__main__":
+    cli()
---
+++ bin/gridnotify.py
@@ -0,0 +1,55 @@
+import rich_click as click
+import re
+
+
+class EmailParamType(click.ParamType):
+    name = 'address'
+
+    def convert(self, value, param, ctx):
+        if not re.match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", value):
+            self.fail(f"'{value}' is not a valid email address.", param, ctx)
+        return value
+
+
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.option(
+    "-l", "--list",
+    default=False,
+    is_flag=True,
+    help="List notifications"
+)
+@click.option(
+    "-m", "--mail",
+    type=EmailParamType(),
+    default=None,
+    help="Subscribe to e-mail notifications with the given e-mail address"
+)
+@click.option(
+    "-u", "--unsubscribe",
+    default=False,
+    is_flag=True,
+    help="Unsubscribe from the specified notifications"
+)
+@click.option(
+    "-s", "--severity",
+    type=click.Choice(["low", "medium", "high"]),
+    default=None,
+    help="Set the severity of notifications to low, medium or high"
+)
+@click.option(
+    "-v", "--verbose",
+    default=False,
+    show_default=True,
+    is_flag=True,
+    help="Enable verbose mode"
+)
+@click.version_option("4.0.0", prog_name="CiGri")
+def cli(list, mail, unsubscribe, severity, verbose):
+    """
+    This command allow the user to manage the notification.
+    """
+    raise RuntimeError("NYI")
+
+
+if __name__ == "__main__":
+    cli()
