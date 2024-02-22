b5526edf958f7b9c2610e10cfe130fe63ac917d3
delast <tanguy.delas@etu.univ-grenoble-alpes.fr>
2024-02-22 10:23:04 +0100

    update killcampaign

--- SQL/queries.py
+++ SQL/queries.py
@@ -6,16 +6,28 @@ engine = create_engine("sqlite+pysqlite:///mydb", echo=True)
 
 #delete(campagnes).where()
 
-killCampaign = update(campagnes).where(campagnes.c.id == 1).values(state = "killed") # should be contrained and not be updated
+def killCampaign (id = 1):
+    killCampaign = update(campagnes).where(campagnes.c.id == id).values(state = "test2") # should be contrained and not be updated
+    with engine.connect() as connect:
+        connect.execute(killCampaign)
+        connect.commit()
 
 #addParam = insert(parameters).values(campaing_id = , name = , param = )
-
+''''
 with engine.connect() as conn:
     #conn.execute(killCampaign)
     #conn.execute(addParam)
     conn.commit()
-    '''
+    
     result = conn.execute(someSelectQuery)
     conn.execute(someQuery)
     conn.commit()
     '''
+
+if __name__ == "__main__":
+    killCampaign()
+
+
+    ###
+    #   transformer les queries en fonction gros porc
+    ###
