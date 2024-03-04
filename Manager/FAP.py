4c3175fc17c3b9c63c1544efc29690176c9c0aaf
Killian Gricourt <gricourt.killian@gmail.com>
2024-03-04 15:46:38 +0100

    Add FAP

---
+++ Manager/FAP.py
@@ -0,0 +1,25 @@
+
+# Higher priority = Higher number
+
+class FAP:
+
+    def __init__(self):
+        self.elements = []
+
+    def append(self, value, priority=0):
+        self.elements.append(FAP.FAPElement(value, priority))
+
+    def next(self):
+        highestPriority = max([p.priority for p in self.elements])
+        lastElement = [e for e in self.elements if e.priority == highestPriority][0]
+        self.elements.remove(lastElement)
+        return lastElement.value
+
+    class FAPElement:
+        def __init__(self, value, priority):
+            self.value = value
+            self.priority = priority
+
+        def set(self, value, priority):
+            self.value = value
+            self.priority = priority
