e48f78c93f5494b2434ce6eb5ec68d963248cc5a
Killian Gricourt <gricourt.killian@gmail.com>
2024-03-11 18:41:48 +0100

    Add gridclusters API route with FastAPI

--- utils/sql.py
+++ utils/sql.py
@@ -4,9 +4,9 @@ path_DB = "sqlite+pysqlite:///SQL/mycigri.db"
 # To use in Test folder
 path_DB_test = "sqlite+pysqlite:///../Test/SQL/mycigri_test.db"
 
-def validation (value, default):
+
+def validation(value, default):
     if value is None or value <= 0:
         return default
     else:
         return value
-
