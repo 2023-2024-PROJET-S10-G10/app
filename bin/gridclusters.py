9e6a81835f1b0b39c6f0dfa5d4694a9f650f22ba
Killian Gricourt <gricourt.killian@gmail.com>
2024-03-11 18:46:18 +0100

    Modify gridcluster to handle APIClient correctly

--- bin/gridclusters.py
+++ bin/gridclusters.py
@@ -3,7 +3,7 @@ import json
 
 import sys
 
-sys.path.append('../')
+sys.path.append("../")
 from API.apiclient import ApiClient
 
 click.rich_click.OPTION_GROUPS = {
@@ -59,9 +59,7 @@ def cli(infos, more_infos, usage, bars):
     This command allow the user to gatheir information on the different clusters.
     """
 
-    client = ApiClient(host="localhost", port=4430, baseEndpoint='/cigri-api',
-                   headers={"Content-Type": "application/json",
-                            "Accept": "application/json"})
+    client = ApiClient(host="localhost", port=8000)
 
     gridclusters(infos, more_infos, usage, bars, client)
 
@@ -81,66 +79,104 @@ def gridclusters(infos, more_infos, usage, bars, client):
 
     # Function implementation
 
-    content = client.get("/clusters").read()
-    clusters = json.loads(content)['items']
+    _, content = client.get("/clusters")
+    clusters = json.loads(content)["items"]
 
     cluster_usages = ""
     if usage:
-        content = client.get("/gridusage").read()
-        cluster_usages = json.loads(content)['items'][0]['clusters']
+        _, content = client.get("/gridusage")
+        cluster_usages = json.loads(content)["items"][0]["clusters"]
 
     prompt = ""
-    clusters_sorted = sorted(clusters, key=lambda x: int(x['id']))
+    clusters_sorted = sorted(clusters, key=lambda x: int(x["id"]))
 
     for cluster in clusters_sorted:
-        prompt += str(cluster['id']) + ": " + cluster["name"]
+        prompt += str(cluster["id"]) + ": " + cluster["name"]
 
         if infos:
-            content = client.get("/clusters/" + cluster['id']).read()
+            _, content = client.get("/clusters/" + cluster["id"])
             cluster_detail = json.loads(content)
 
             if more_infos:
                 for key in cluster_detail:
                     if key != "links":
-                        prompt += "\n\t" + key + ": " + str(cluster_detail[key])
+                        prompt += (
+                            "\n\t" + key + ": " + str(cluster_detail[key])
+                        )
             else:
-                prompt += ", " + cluster_detail['ssh_host'] + " (stress:" + cluster_detail['stress_factor']
-                if cluster_detail['blacklisted']:
+                prompt += (
+                    ", "
+                    + cluster_detail["ssh_host"]
+                    + " (stress:"
+                    + cluster_detail["stress_factor"]
+                )
+                if cluster_detail["blacklisted"]:
                     prompt += ", BLACKLISTED"
-                if cluster_detail['under_stress']:
+                if cluster_detail["under_stress"]:
                     prompt += ", UNDER_STRESS"
                 prompt += ")"
             if usage:
-                if int(cluster['id']) not in [cluster_usage['cluster_id'] for cluster_usage in cluster_usages]:
+                if int(cluster["id"]) not in [
+                    cluster_usage["cluster_id"]
+                    for cluster_usage in cluster_usages
+                ]:
                     prompt += "\n\tData temporarily unavailable\n"
                 else:
                     for cluster_usage in cluster_usages:
-                        if not cluster_detail['blacklisted']:
-                            if int(cluster['id']) == cluster_usage['cluster_id']:
+                        if not cluster_detail["blacklisted"]:
+                            if (
+                                int(cluster["id"])
+                                == cluster_usage["cluster_id"]
+                            ):
                                 if bars:
                                     size = 80
-                                    unavailable = cluster_usage["unavailable_resources"]
+                                    unavailable = cluster_usage[
+                                        "unavailable_resources"
+                                    ]
                                     used = cluster_usage["used_resources"]
                                     cigri = cluster_usage["used_by_cigri"]
                                     used -= cigri
                                     max_value = cluster_usage["max_resources"]
-                                    free = max_value - cigri - used - unavailable
+                                    free = (
+                                        max_value - cigri - used - unavailable
+                                    )
                                     prompt += f" ({max_value} resources)\n"
-                                    for _ in range(int(round(unavailable * size / max_value))):
+                                    for _ in range(
+                                        int(
+                                            round(
+                                                unavailable * size / max_value
+                                            )
+                                        )
+                                    ):
                                         prompt += "\033[41m \033[0m"  # red
 
-                                    for _ in range(int(round(used * size / max_value))):
+                                    for _ in range(
+                                        int(round(used * size / max_value))
+                                    ):
                                         prompt += "\033[43m \033[0m"  # yellow
 
-                                    for _ in range(int(round(cigri * size / max_value))):
+                                    for _ in range(
+                                        int(round(cigri * size / max_value))
+                                    ):
                                         prompt += "\033[47m \033[0m"  # white
 
-                                    for _ in range(int(round(free * size / max_value))):
+                                    for _ in range(
+                                        int(round(free * size / max_value))
+                                    ):
                                         prompt += "\033[42m \033[0m"  # green
                                 else:
                                     for key in cluster_usage:
-                                        if key != "cluster_id" and key != "cluster_name":
-                                            prompt += "\n\t" + key + ": " + str(cluster_usage[key]) + "\n"
+                                        if (
+                                            key != "cluster_id"
+                                            and key != "cluster_name"
+                                        ):
+                                            prompt += (
+                                                "\n\t"
+                                                + key
+                                                + ": "
+                                                + str(cluster_usage[key])
+                                                + "\n"
+                                            )
                                 prompt += "\n"
         prompt += "\n"
     if bars:
