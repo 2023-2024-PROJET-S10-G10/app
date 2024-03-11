7d7502d8ee3b55889b5e8f5d3871dbc8eaf7114b
Diego CORREIA DE OLIVEIRA <Diego.cdo27@gmail.com>
2024-03-11 09:40:14 +0100

    Feat: gridsub

--- bin/gridsub.py
+++ bin/gridsub.py
@@ -1,4 +1,10 @@
 import rich_click as click
+import json
+
+import sys
+
+sys.path.append("../")
+from API.apiclient import ApiClient
 
 click.rich_click.OPTION_GROUPS = {
     "gridsub.py": [
@@ -57,7 +63,9 @@ class JSON(str):
     default=None,
     help="JDL File containing an array of parameters",
 )
-@click.option("-j", "--json", type=JSON, default=None, help="JSON String")
+@click.option(
+    "-j", "--json", "jsonCLI", type=JSON, default=None, help="JSON String"
+)
 @click.option(
     "-J",
     "--jsonjob",
@@ -66,22 +74,101 @@ class JSON(str):
     help="JSON String containing an array of parameters",
 )
 @click.version_option("4.0.0", prog_name="CiGri")
-def cli(campaign, details, file, jobfile, json, jsonjob):
+def cli(campaign, details, file, jobfile, jsonCLI, jsonjob):
     """
     This command allow the user to submit campaigns to CiGri.
     """
 
+    client = ApiClient(
+        host="localhost",
+        port=4430,
+        baseEndpoint="/cigri-api",
+        headers={
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+    )
+
+    gridsub(campaign, details, file, jobfile, jsonCLI, jsonjob, client)
+
+
+def submit_campaign(jdl, client, campaign_id=None):
+    url = "/campaigns"
+
+    if campaign_id is not None:
+        url += f"/{campaign_id}/jobs"
+
+    return client.post(url, body=jdl)
+
+
+def gridsub(campaign, details, file, jobfile, jsonCLI, jsonjob, client):
     # Checking option content
 
-    if file is None and jobfile is None and json is None and jsonjob is None:
+    if (
+        file is None
+        and jobfile is None
+        and jsonCLI is None
+        and jsonjob is None
+    ):
         raise click.MissingParameter(
             "Must provide one of the four --file, --jobfile, --json, --jsonjob option",
             param_type="option",
         )
 
+    if (jobfile is not None or jsonjob is not None) and campaign is None:
+        raise click.MissingParameter(
+            "Must provide a campaign id using --campaign when using --jobfile or --jsonjob",
+            param_type="option",
+        )
+
     # Function implementation
 
-    raise RuntimeError("NYI")
+    jsons = []
+    jobs = []
+
+    if file is not None:
+        with open(file) as f:
+            jsons.append(f.read())
+
+    if jobfile is not None:
+        with open(jobfile) as f:
+            jobs.append(f.read())
+
+    if jsonCLI is not None:
+        jsons.append(jsonCLI)
+
+    if jsonjob is not None:
+        jobs.append(jsonjob)
+
+    for json_ in jsons:
+        response = submit_campaign(json_, client)
+        body = response.read()
+        if response.status == 201:
+            print("Campaign successfully submitted")
+            if details:
+                print(json.dumps(json.loads(body.decode()), indent=2))
+        else:
+            try:
+                error = json.loads(body)
+                if error["title"] == "Admission rule error":
+                    print(f"Admission rule error:\n\t{error['message']}")
+                else:
+                    raise RuntimeError(f"Error: {response.body}")
+            except json.decoder.JSONDecodeErrors:
+                raise RuntimeError(f"Error: {body}")
+
+    for job in jobs:
+        response = submit_campaign(job, client, campaign_id=campaign)
+        body = response.read()
+
+        if response.status == 201:
+            print(f"Jobs successfully added to campaign {campaign}")
+            if details:
+                print(json.dumps(json.loads(body.decode()), indent=2))
+        else:
+            raise RuntimeError(
+                f"Error adding jobs to the campaign ({campaign}): {body}"
+            )
 
 
 if __name__ == "__main__":
